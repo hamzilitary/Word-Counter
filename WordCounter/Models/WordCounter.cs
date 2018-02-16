e8043ed1c99a471197a25fcaf8a52703328ac849
Guest User <Guest@Epicodus-7E.local>
2018-02-16 11:45:07 -0800

    add Models before Testing

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -2,3 +2,37 @@ using System.Collections.Generic;
 using System;
 
 namespace WordCounters.Models
+{
+  public class WordCounter
+  {
+    private string _text;
+    private string _find;
+    private string _result;
+
+    public WordCounter(string text, string find)
+    {
+      _text = text;
+      _find = find;
+    }
+
+    public string GetText()
+    {
+      return _text;
+    }
+    public string GetFind()
+    {
+      return _find;
+    }
+    public string GetResult()
+    {
+      return _result;
+    }
+    public int GetIndex(int start)
+    {
+      string text = _text.Substring(start, _text.Length - start).ToLower();
+      return text.IndexOf(_find.ToLower()) + start;
+    }
+
+  }
+
+}
